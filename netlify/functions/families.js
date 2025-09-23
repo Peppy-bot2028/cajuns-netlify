150efc3f037296092cc6e1f86034dadc23e31ded
Peppy-bot2028 <prechter1@gmail.com>
2025-09-23 18:05:23 -0500

    Delete netlify/functions directory

--- netlify/functions/families.js
+++
@@ -1,9 +0,0 @@
-import { getStore } from "@netlify/blobs";
-
-export default async (req) => {
-  const store = getStore("cajuns");
-  const families = JSON.parse((await store.get("families")) || "[]");
-  return new Response(JSON.stringify(families), {
-    headers: { "content-type": "application/json" }
-  });
-};
