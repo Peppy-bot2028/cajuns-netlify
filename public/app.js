e6aa09e7a974b074677eddc227d10b81e73684f1
Peppy-bot2028 <prechter1@gmail.com>
2025-09-23 18:05:33 -0500

    Delete public directory

--- public/app.js
+++
@@ -1,5 +0,0 @@
-async function searchFamily(name) {
-  const res = await fetch(`/.netlify/functions/families?search=${encodeURIComponent(name)}`);
-  const data = await res.json();
-  console.log(data);
-}
